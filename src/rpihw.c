6cc26f841122e33398aabc23f03340672c7e5271
TangoCash <eric@loxat.de>
2024-12-04 17:16:10 +0100

    Update rpihw.c

--- src/rpihw.c
+++ src/rpihw.c
@@ -63,6 +63,14 @@ static const rpi_hw_t rpi_hw_info[] =
 		.videocore_base = VIDEOCORE_BASE_RPI2,
 		.desc = "Pi 400 - 4GB v1.0"
 	},
+	{
+		.hwver = 0xc03131,
+		.type = RPI_HWVER_TYPE_PI4,
+		.periph_base = PERIPH_BASE_RPI4,
+		.videocore_base = VIDEOCORE_BASE_RPI2,
+		.desc = "Pi 400 - 4GB v1.1"
+	},
+
 	//
 	// Raspberry Pi 4
 	//
@@ -109,19 +117,115 @@ static const rpi_hw_t rpi_hw_info[] =
 		.desc = "Pi 4 Model B - 4GB v1.2"
 	},
 	{
-		.hwver = 0xD03114,
-		.type = RPI_HWVER_TYPE_PI4,
-		.periph_base = PERIPH_BASE_RPI4,
-		.videocore_base = VIDEOCORE_BASE_RPI2,
-		.desc = "Pi 4 Model B - 8GB v1.2"
-	},
-	{
 		.hwver = 0xb03114,
 		.type = RPI_HWVER_TYPE_PI4,
 		.periph_base = PERIPH_BASE_RPI4,
 		.videocore_base = VIDEOCORE_BASE_RPI2,
 		.desc = "Pi 4 Model B - 2GB v1.4"
 	},
+	{
+		.hwver = 0xD03114,
+		.type = RPI_HWVER_TYPE_PI4,
+		.periph_base = PERIPH_BASE_RPI4,
+		.videocore_base = VIDEOCORE_BASE_RPI2,
+		.desc = "Pi 4 Model B - 8GB v1.4"
+	},
+	{
+		.hwver = 0xc03114,
+		.type = RPI_HWVER_TYPE_PI4,
+		.periph_base = PERIPH_BASE_RPI4,
+		.videocore_base = VIDEOCORE_BASE_RPI2,
+		.desc = "Pi 4 Model B - 4GB v1.4"
+	},
+	{
+		.hwver = 0xa03115,
+		.type = RPI_HWVER_TYPE_PI4,
+		.periph_base = PERIPH_BASE_RPI4,
+		.videocore_base = VIDEOCORE_BASE_RPI2,
+		.desc = "Pi 4 Model B - 1GB v1.5"
+	},
+	{
+		.hwver = 0xb03115,
+		.type = RPI_HWVER_TYPE_PI4,
+		.periph_base = PERIPH_BASE_RPI4,
+		.videocore_base = VIDEOCORE_BASE_RPI2,
+		.desc = "Pi 4 Model B - 2GB v1.5"
+	},
+	{
+		.hwver = 0xc03115,
+		.type = RPI_HWVER_TYPE_PI4,
+		.periph_base = PERIPH_BASE_RPI4,
+		.videocore_base = VIDEOCORE_BASE_RPI2,
+		.desc = "Pi 4 Model B - 4GB v1.5"
+	},
+	{
+		.hwver = 0xd03115,
+		.type = RPI_HWVER_TYPE_PI4,
+		.periph_base = PERIPH_BASE_RPI4,
+		.videocore_base = VIDEOCORE_BASE_RPI2,
+		.desc = "Pi 4 Model B - 8GB v1.5"
+	},
+
+	//
+	// Compute Module 4
+	//
+	{
+		.hwver = 0xa03140,
+		.type = RPI_HWVER_TYPE_PI4,
+		.periph_base = PERIPH_BASE_RPI4,
+		.videocore_base = VIDEOCORE_BASE_RPI2,
+		.desc = "Compute Module 4 v1.0 eMMC"
+	},
+	{
+		.hwver = 0xb03140,
+		.type = RPI_HWVER_TYPE_PI4,
+		.periph_base = PERIPH_BASE_RPI4,
+		.videocore_base = VIDEOCORE_BASE_RPI2,
+		.desc = "Compute Module 4 v1.0 Lite"
+	},
+	{
+		.hwver = 0xc03140,
+		.type = RPI_HWVER_TYPE_PI4,
+		.periph_base = PERIPH_BASE_RPI4,
+		.videocore_base = VIDEOCORE_BASE_RPI2,
+		.desc = "Compute Module 4 v1.0 WiFi"
+	},
+	{
+		.hwver = 0xd03140,
+		.type = RPI_HWVER_TYPE_PI4,
+		.periph_base = PERIPH_BASE_RPI4,
+		.videocore_base = VIDEOCORE_BASE_RPI2,
+		.desc = "Compute Module 4 v1.0 WiFi 8GB"
+	},
+	{
+		.hwver = 0xa03141,
+		.type = RPI_HWVER_TYPE_PI4,
+		.periph_base = PERIPH_BASE_RPI4,
+		.videocore_base = VIDEOCORE_BASE_RPI2,
+		.desc = "Compute Module 4 Rev 1.1"
+	},
+	{
+		.hwver = 0xb03141,
+		.type = RPI_HWVER_TYPE_PI4,
+		.periph_base = PERIPH_BASE_RPI4,
+		.videocore_base = VIDEOCORE_BASE_RPI2,
+		.desc = "Compute Module 4 Rev 1.1"
+	},
+	{
+		.hwver = 0xc03141,
+		.type = RPI_HWVER_TYPE_PI4,
+		.periph_base = PERIPH_BASE_RPI4,
+		.videocore_base = VIDEOCORE_BASE_RPI2,
+		.desc = "Compute Module 4 Rev 1.1"
+	},
+	{
+		.hwver = 0xd03141,
+		.type = RPI_HWVER_TYPE_PI4,
+		.periph_base = PERIPH_BASE_RPI4,
+		.videocore_base = VIDEOCORE_BASE_RPI2,
+		.desc = "Compute Module 4 Rev 1.1"
+	},
+
 	//
 	// Model B Rev 1.0
 	//
@@ -257,6 +361,27 @@ static const rpi_hw_t rpi_hw_info[] =
 		.videocore_base = VIDEOCORE_BASE_RPI,
 		.desc = "Compute Module 1",
 	},
+	{
+		.hwver  = 0xa020a0,
+		.type = RPI_HWVER_TYPE_PI2,
+		.periph_base = PERIPH_BASE_RPI2,
+		.videocore_base = VIDEOCORE_BASE_RPI2,
+		.desc = "Compute Module 3/L3",
+	},
+	{
+		.hwver  = 0xa02100,
+		.type = RPI_HWVER_TYPE_PI2,
+		.periph_base = PERIPH_BASE_RPI2,
+		.videocore_base = VIDEOCORE_BASE_RPI2,
+		.desc = "Compute Module 3+",
+	},
+	{
+		.hwver  = 0xa220a0,
+		.type = RPI_HWVER_TYPE_PI2,
+		.periph_base = PERIPH_BASE_RPI2,
+		.videocore_base = VIDEOCORE_BASE_RPI2,
+		.desc = "Compute Module 3 Rev 1.0",
+	},
 
 	//
 	// Pi Zero
@@ -295,6 +420,17 @@ static const rpi_hw_t rpi_hw_info[] =
 		.periph_base = PERIPH_BASE_RPI,
 		.videocore_base = VIDEOCORE_BASE_RPI,
 		.desc = "Pi Zero W v1.1",
+	},
+
+	//
+	// Model Zero 2 W
+	//
+	{
+		.hwver  = 0x902120,
+		.type = RPI_HWVER_TYPE_PI2,
+		.periph_base = PERIPH_BASE_RPI2,
+		.videocore_base = VIDEOCORE_BASE_RPI2,
+		.desc = "Pi Zero 2 W v1.0",
 	},
 
 	//
@@ -360,11 +496,32 @@ static const rpi_hw_t rpi_hw_info[] =
 	// Pi 3 Model B
 	//
 	{
+		.hwver  = 0xa020d4,
+		.type = RPI_HWVER_TYPE_PI2,
+		.periph_base = PERIPH_BASE_RPI2,
+		.videocore_base = VIDEOCORE_BASE_RPI2,
+		.desc = "Pi 3 B+",
+	},
+	{
 		.hwver  = 0xa020d3,
 		.type = RPI_HWVER_TYPE_PI2,
 		.periph_base = PERIPH_BASE_RPI2,
 		.videocore_base = VIDEOCORE_BASE_RPI2,
 		.desc = "Pi 3 B+",
+	},
+	{
+		.hwver  = 0xa32082,
+		.type = RPI_HWVER_TYPE_PI2,
+		.periph_base = PERIPH_BASE_RPI2,
+		.videocore_base = VIDEOCORE_BASE_RPI2,
+		.desc = "Pi 3 B",
+	},
+	{
+		.hwver  = 0xa52082,
+		.type = RPI_HWVER_TYPE_PI2,
+		.periph_base = PERIPH_BASE_RPI2,
+		.videocore_base = VIDEOCORE_BASE_RPI2,
+		.desc = "Pi 3 B",
 	},
 	{
 		.hwver  = 0xa02082,
@@ -401,29 +558,13 @@ static const rpi_hw_t rpi_hw_info[] =
 		.videocore_base = VIDEOCORE_BASE_RPI2,
 		.desc = "Model 3 A+",
 	},
-
-	//
-	// Pi Compute Module 3
-	//
 	{
-		.hwver  = 0xa020a0,
+		.hwver  = 0x9020e1,
 		.type = RPI_HWVER_TYPE_PI2,
 		.periph_base = PERIPH_BASE_RPI2,
 		.videocore_base = VIDEOCORE_BASE_RPI2,
-		.desc = "Compute Module 3/L3",
-	},
-	//
-	// Pi Compute Module 3+
-	//
-	{
-		.hwver  = 0xa02100,
-		.type = RPI_HWVER_TYPE_PI2,
-		.periph_base = PERIPH_BASE_RPI2,
-		.videocore_base = VIDEOCORE_BASE_RPI2,
-		.desc = "Compute Module 3+",
-	},
-
-
+		.desc = "Model 3 A+",
+	}
 };
 
 
@@ -509,4 +650,3 @@ done:
 
 	return result;
 }
-
